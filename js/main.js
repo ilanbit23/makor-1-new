4d17bfd5729317f5b24a6311e6119d470cca7846
Ilan bit <ilanbit23@gmail.com>
2020-10-08 01:31:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,12 +63,12 @@ function generate(event) {
     news.style.letterSpacing = '0.2px';
     document.querySelector('.social').style.display = 'none';
     document.querySelector('.logo').style.display = 'none';
-    window.scrollTo(-350, 0);
+    window.scrollTo(-400, 0);
     html2canvas(document.querySelector(".output"), {
         scale: 1,
     }).then(canvas => {
         document.querySelector('.canvas-container').appendChild(canvas);
-        canvas.style.display = "none";
+        //canvas.style.display = "none";
         document.querySelector('.social').style.display = 'flex';
         document.querySelector('.logo').style.display = 'block';
     });
